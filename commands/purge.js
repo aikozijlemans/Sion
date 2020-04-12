6fed827494c4f0e8d7cdefbbbd97bc2570ab9ab6
Jeremy <58021840+jeremyzijlemans@users.noreply.github.com>
2020-04-12 18:07:26 -0400

    purge.js

---
+++ commands/purge.js
@@ -0,0 +1,20 @@
+exports.run = (client, message, args) => {
+    const messagecount = parseInt(args.join(' '));
+    message.channel.fetchMessages({
+      limit: messagecount
+    }).then(messages => message.channel.bulkDelete(messages));
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "Administrator"
+  };
+  
+  exports.help = {
+    name: 'purge',
+    category: "Admin Commands",
+    description: 'Purges X amount of messages from a given channel.',
+    usage: 'purge <number>'
+  };
