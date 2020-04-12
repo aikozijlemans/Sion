f0189dd904057516b7f71798438f5f31a795578d
Jeremy <58021840+jeremyzijlemans@users.noreply.github.com>
2020-04-12 17:52:38 -0400

    ass.js

---
+++ commands/ass.js
@@ -0,0 +1,28 @@
+const discord = require('discord.js');
+const superagent = require('superagent')
+
+exports.run = (client, msg, args) => {
+  if (msg.channel.nsfw === true) {
+    superagent.get('https://nekobot.xyz/api/image')
+    .query({ type: 'ass'})
+    .end((err, response) => {
+      msg.channel.send({ file: response.body.message });
+    });
+  } else {
+    msg.channel.send("This isn't NSFW channel!")
+  }
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["Ass", "ass","ASS"],
+    permLevel: "User"
+  };
+  
+  exports.help = {
+    name: 'ass',
+    category: "NSFW",
+    description: 'shows you that booty',
+    usage: 'ass, Ass, ASS'
+  };
