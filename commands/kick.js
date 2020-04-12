743a6dd11d230e9804c3ae4c080c838b2012d2d3
Jeremy <58021840+jeremyzijlemans@users.noreply.github.com>
2020-04-12 18:03:33 -0400

    kick.js

---
+++ commands/kick.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+  const reason = args.slice(1).join(' ');
+  const user = message.mentions.users.first();
+  const modlog = client.channels.find('name', 'mod-log');
+  if (!modlog) return message.reply('I cannot find a mod-log channel');
+  if (reason.length < 1) return message.reply('You must supply a reason for the kick.');
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to kick them.').catch(console.error);
+
+  if (!message.guild.member(user).kickable) return message.reply('I cannot kick that member');
+  message.guild.member(user).kick();
+
+  const embed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setDescription(`**Action:** Kick\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`);
+  return client.channels.get(modlog.id).send({embed});
+};
+
+exports.conf = {
+  aliases: [],
+  permLevel: "Administrator"
+};
+
+exports.help = {
+  name: 'kick',
+  category: "Admin Commands",
+  description: 'Kicks the mentioned user.',
+  usage: 'kick [mention] [reason]'
+};
