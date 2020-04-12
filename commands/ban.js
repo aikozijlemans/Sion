0a3720cf205791ae2cca36901558cf60a116c085
Jeremy <58021840+jeremyzijlemans@users.noreply.github.com>
2020-04-12 17:52:57 -0400

    ban.js

---
+++ commands/ban.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+  let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  let modlog = client.channels.find('name', 'mod-log');
+  if (!modlog) return message.reply('I cannot find a mod-log channel');
+  if (reason.length < 1) return message.reply('You must supply a reason for the ban.');
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to ban them.').catch(console.error);
+
+  if (!message.guild.member(user).bannable) return message.reply('I cannot ban that member');
+  message.guild.ban(user, 2);
+
+  const embed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setDescription(`**Action:** Ban\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`);
+  return client.channels.get(modlog.id).send({embed});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "Administrator"
+};
+
+exports.help = {
+  name: 'ban',
+  category: "Admin Commands",
+  description: 'Bans the mentioned user.',
+  usage: 'ban [mention] [reason]'
+};
