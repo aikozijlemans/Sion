a0c52fe933889f4ff0222e419a1bef5c65c2bd4e
Jeremy <58021840+jeremyzijlemans@users.noreply.github.com>
2020-04-12 15:45:11 -0400

    config.js

---
+++ config.js
@@ -0,0 +1,68 @@
+const config = {
+  "ownerID": "Censored",
+  "admins": ["Censored","Censored","Censored","Censored","Censored"],
+  "support": ["Censored","Censored"],
+  "token": "Censored",
+  
+  "defaultSettings" : {
+    "prefix": "$",
+    "modLogChannel": "mod-log",
+    "modRole": "Moderator",
+    "adminRole": "Administrator",
+    "systemNotice": "true", 
+    "welcomeChannel": "welcome",
+    "welcomeMessage": "Say hello to {{user}}, everyone! We all need a warm welcome sometimes :D",
+    "welcomeEnabled": "true"
+  },
+
+
+  permLevels: [
+    { level: 0,
+      name: "User", 
+      check: () => true
+    },
+
+    { level: 2,
+      name: "Moderator",
+
+      check: (message) => {
+        try {
+          const modRole = message.guild.roles.find(r => r.name.toLowerCase() === message.settings.modRole.toLowerCase());
+          if (modRole && message.member.roles.has(modRole.id)) return true;
+        } catch (e) {
+          return false;
+        }
+      }
+    },
+
+    { level: 3,
+      name: "Administrator", 
+      check: (message) => {
+        try {
+          const adminRole = message.guild.roles.find(r => r.name.toLowerCase() === message.settings.adminRole.toLowerCase());
+          return (adminRole && message.member.roles.has(adminRole.id));
+        } catch (e) {
+          return false;
+        }
+      }
+    },
+    { level: 4,
+      name: "Server Owner", 
+      check: (message) => message.channel.type === "text" ? (message.guild.ownerID === message.author.id ? true : false) : false
+    },
+    { level: 8,
+      name: "Bot Support",
+      check: (message) => config.support.includes(message.author.id)
+    },
+    { level: 9,
+      name: "Bot Admin",
+      check: (message) => config.admins.includes(message.author.id)
+    },
+    { level: 10,
+      name: "Bot Owner", 
+      check: (message) => message.client.config.ownerID === message.author.id
+    }
+  ]
+};
+
+module.exports = config;
