8349586710b59bc7b351414c9b59f3d614239172
Jeremy <58021840+jeremyzijlemans@users.noreply.github.com>
2020-04-12 18:09:26 -0400

    SCP.js

---
+++ commands/SCP.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) => {
+    let msg = await message.channel.send("Generating...")
+    var { body } = await superagent
+    .get('https://meme-api.herokuapp.com/gimme/dankmemesfromsite19')
+    console.log(body.url)
+    if(!body) return message.channel.send("I broke! Try again.")
+
+        let mEmbed = new Discord.RichEmbed()
+        .setAuthor('SCP', message.guild.iconURL)
+        .setImage(body.url)
+        .setTimestamp()
+
+        message.channel.send(mEmbed)
+        msg.delete();
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["SCP", "scp"],
+    permLevel: "User"
+  };
+  
+  exports.help = {
+    name: 'SCP',
+    category: "fun",
+    description: "Special containment protocol",
+    usage: 'SCP,scp'
+  };
