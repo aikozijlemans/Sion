1872d8835c403f286f405b4ee3de9063cd24aeb0
Jeremy <58021840+jeremyzijlemans@users.noreply.github.com>
2020-04-12 18:05:02 -0400

    mute.js

---
+++ commands/mute.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+  const reason = args.slice(1).join(' ');
+  const user = message.mentions.users.first();
+  const modlog = client.channels.find('name', 'mod-log');
+  const muteRole = client.guilds.get(message.guild.id).roles.find('name', 'muted');
+  if (!modlog) return message.reply('I cannot find a mod-log channel').catch(console.error);
+  if (!muteRole) return message.reply('I cannot find a mute role').catch(console.error);
+  if (reason.length < 1) return message.reply('You must supply a reason for the mute.').catch(console.error);
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to mute them.').catch(console.error);
+  const embed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setDescription(`**Action:** Un/mute\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`);
+
+  if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.reply('I do not have the correct permissions.').catch(console.error);
+
+  if (message.guild.member(user).roles.has(muteRole.id)) {
+    message.guild.member(user).removeRole(muteRole).then(() => {
+      client.channels.get(modlog.id).send({embed}).catch(console.error);
+    });
+  } else {
+    message.guild.member(user).addRole(muteRole).then(() => {
+      client.channels.get(modlog.id).send({embed}).catch(console.error);
+    });
+  }
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['unmute'],
+  permLevel: "Administrator"
+};
+
+exports.help = {
+  name: 'mute',
+  category: "Admin Commands",
+  description: 'mutes or unmutes a mentioned user',
+  usage: 'un/mute [mention] [reason]'
+};
